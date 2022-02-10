f3a1e3307a1ae9fd60873c5629eb9120f4b8c1f7
hulk510 <dorian.51069@gmail.com>
2022-02-10 18:38:48 +0900

    Initial Commit

---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by 後藤遥 on 2022/02/10.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
